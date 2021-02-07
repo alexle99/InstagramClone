a81ee409ec048d83baf5889ae0036e1fb7ec0553
Alexander Le <alexanvl@uci.edu>
2021-02-06 23:14:15 -0800

    initial commit, completed read me and gif

---
+++ InstagramClone/app/src/main/java/com/example/instagramclone/ParseApplication.java
@@ -0,0 +1,23 @@
+package com.example.instagramclone;
+
+import android.app.Application;
+
+import com.parse.Parse;
+import com.parse.ParseObject;
+
+public class ParseApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // Register your parse models
+        ParseObject.registerSubclass(Post.class);
+
+        Parse.initialize(new Parse.Configuration.Builder(this)
+                .applicationId("Kc36PBcfAaIPSDKUnBvnsN7iewDSwt37J0DKSszq")
+                .clientKey("GKbi0fqMD3S4wW9EFKEnEJDntlRIRVO175N0MPhY")
+                .server("https://parseapi.back4app.com")
+                .build()
+        );
+    }
+}
